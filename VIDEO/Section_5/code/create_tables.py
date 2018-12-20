761793e9451d19a29d5b58446eacaa481a3640c4
Tyler Pritchard <pritchard.tyler@gmail.com>
2018-12-20 13:52:14 -0800

    add

---
+++ VIDEO/Section_5/code/create_tables.py
@@ -0,0 +1,12 @@
+import sqlite3
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+
+# use INTEGER (full word, not "int") to make auto-incrementing ID values
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username test, password text)"
+cursor.execute(create_table)
+
+connection.commmit()
+
+connection.close()

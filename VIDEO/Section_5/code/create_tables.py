b25883f01f3bfd022c1324b6615e95655742776e
Tyler Pritchard <pritchard.tyler@gmail.com>
2018-12-21 08:54:03 -0800

    m

--- VIDEO/Section_5/code/create_tables.py
+++ VIDEO/Section_5/code/create_tables.py
@@ -7,6 +7,9 @@ cursor = connection.cursor()
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username test, password text)"
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute("INSERT INTO items VALUES ('test': 10.99)")
+
 connection.commit()
 
 connection.close()
